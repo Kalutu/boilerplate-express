40bdde67a2bdd6990620623efcdf864f6d19b431
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 16:59:03 +0300

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,13 @@ let app = express();
 
 app.use("/public", express.static(__dirname + '/public'));
 
+
+app.use(function(req, res, next){
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+})
+
+
 app.get("/",function(req,res){
   res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
@@ -16,7 +23,6 @@ app.get("/json",function(req,res){
   }
   
 });
-
 
 
 
