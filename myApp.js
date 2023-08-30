3a5a0004bbe6b1d921ec25a9bb411a233158b0e2
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 08:29:10 +0300

    Serve static assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(__dirname + '/public'));
+
 app.get("/",function(req,res){
   res.sendFile(absolutePath = __dirname + '/views/index.html');
 })
+
+
 
 
 
