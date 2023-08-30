81a4804b9eba7b9fcca13abbf4cd4aaf54809f74
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 09:09:19 +0300

    use .env file

--- myApp.js
+++ myApp.js
@@ -8,7 +8,13 @@ app.get("/",function(req,res){
 });
 
 app.get("/json",function(req,res){
-  res.json({"message": "Hello json"});
+  if(process.env["MESSAGE_STYLE"]=="uppercase"){
+    res.json({"message": "HELLO JSON"});
+  }
+  else{
+    res.json({"message": "Hello json"});
+  }
+  
 });
 
 
