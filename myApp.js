d822d99da188e824763e4468c0a1e640bd44e0a3
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 08:46:46 +0300

    Serve json

--- myApp.js
+++ myApp.js
@@ -5,7 +5,11 @@ app.use("/public", express.static(__dirname + '/public'));
 
 app.get("/",function(req,res){
   res.sendFile(absolutePath = __dirname + '/views/index.html');
-})
+});
+
+app.get("/json",function(req,res){
+  res.json({"message": "Hello json"});
+});
 
 
 
