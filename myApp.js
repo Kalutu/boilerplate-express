85363e3d819aae643bf247c6e5a4f9a934b2fd1c
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 18:35:30 +0300

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
 
-app.use("/public", express.static(__dirname + '/public'));
+let bodyParser = require('body-parser');
 
+app.use("/public", express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next){
   console.log(req.method+" "+req.path+" - "+req.ip);
