1e08fe24a30b9e7a0644da9528d146738a64f1db
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 17:21:01 +0300

    chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -24,6 +24,13 @@ app.get("/json",function(req,res){
   
 });
 
+app.get("/now",function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.json({"time" : req.time});
+})
+
 
 
 
