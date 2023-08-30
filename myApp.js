0ec721e388b8b375e38a674a5b2d5e2e03f093d9
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-08-30 08:04:28 +0300

    Serve a HTML file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
 
 app.get("/",function(req,res){
-  res.send("Hello Express");
+  res.sendFile(absolutePath = __dirname + '/views/index.html');
 })
 
 
